80e1251207fa93f3c8f1db4a678862a7fd64fdb0
brash <s25070@pjwstk.edu.pl>
2024-05-15 11:00:14 +0200

    Created window, renderer and set color background

--- main.cpp
+++ main.cpp
@@ -1,19 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <string>
 #include <SDL.h>
 
 using namespace std;
 
+int main(int argc, char *argv[]) {
 
-int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Init( SDL_INIT_EVERYTHING );
-    auto window = SDL_CreateWindow( "Okienko SDL",
-                                    SDL_WINDOWPOS_UNDEFINED,
-                                    SDL_WINDOWPOS_UNDEFINED,
-                                    640, 480,
-                                    SDL_WINDOW_SHOWN );
-    SDL_Delay(2000);
-    SDL_DestroyWindow( window );
+    //create window and renderer
+    SDL_Window *window = SDL_CreateWindow("Snake",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          640,
+                                          480,
+                                          SDL_WINDOW_SHOWN
+    );
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window,
+                                                -1,
+                                                SDL_RENDERER_ACCELERATED
+    );
+
+
+    // color setting
+    SDL_SetRenderDrawColor(renderer, 0, 50, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+
+    //game
+    bool quit = false;
+    SDL_Event event;
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+
+    // gc
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
-    return 0;
 
+    return 0;
 }
